fb20739eea562b8f7a32a0f9a1bc81f3d3e3ddf1
Vincent Huang <vincom2@gmail.com>
2016-05-01 11:38:30 -0400

    lots of cool tests

--- keymaerax-webui/src/test/scala/unitTypeTests/UnitTypeCheckerTests.scala
+++ keymaerax-webui/src/test/scala/unitTypeTests/UnitTypeCheckerTests.scala
@@ -8,7 +8,7 @@ import edu.cmu.cs.ls.keymaerax.parser.{KeYmaeraXProblemParser, KeYmaeraXProblemP
   * Created by vincenth on 01/05/16.
   */
 class UnitTypeCheckerTests extends TacticTestBase {
-  "UnitChecker" should "check simple unit stuff!" in {
+  "Unit checker" should "check simple unit stuff!" in {
     val input =
       """
         |ProgramUnits.
@@ -26,7 +26,7 @@ class UnitTypeCheckerTests extends TacticTestBase {
     result shouldBe Equal(Variable("x"), Variable("y"))
   }
 
-  "UnitChecker" should "catch simple mismatched units example!" in {
+  it should "catch simple mismatched units example!" in {
     val input =
       """
         |ProgramUnits.
@@ -41,9 +41,136 @@ class UnitTypeCheckerTests extends TacticTestBase {
         | x = y
         |End.
       """.stripMargin
-    val thrown = intercept[Exception] {
+    intercept[ParseException] {
       val (result, whatever) = KeYmaeraXProblemParser(input)
     }
-    assert(thrown.isInstanceOf[ParseException])
   }
+
+  it should "allow products of actual units" in {
+    val input =
+      """
+        |ProgramUnits.
+        | U m.
+        | U n.
+        |End.
+        |ProgramVariables.
+        | R x : m.
+        | R y : n.
+        | R z : m.
+        | R w : n.
+        |End.
+        |Problem.
+        | x * y <= w * z
+        |End.
+      """.stripMargin
+    val (result, _) = KeYmaeraXProblemParser(input)
+    result shouldBe LessEqual(Times(Variable("x"), Variable("y")), Times(Variable("w"), Variable("z")))
+  }
+
+  it should "disallow improper products of actual units" in {
+    val input =
+      """
+        |ProgramUnits.
+        | U m.
+        | U n.
+        |End.
+        |ProgramVariables.
+        | R x : m.
+        | R y : n.
+        | R z : m.
+        | R w : n.
+        |End.
+        |Problem.
+        | x * y = z * z
+        |End.
+      """.stripMargin
+    intercept[ParseException] {
+      val (_, _) = KeYmaeraXProblemParser(input)
+    }
+  }
+
+  it should "allow quotients of actual units" in {
+    val input =
+      """
+        |ProgramUnits.
+        | U m.
+        | U n.
+        |End.
+        |ProgramVariables.
+        | R x : m.
+        | R y : n.
+        | R z : m.
+        | R w : n.
+        |End.
+        |Problem.
+        | x / y = z / w
+        |End.
+      """.stripMargin
+    val (result, _) = KeYmaeraXProblemParser(input)
+    result shouldBe Equal(Divide(Variable("x"), Variable("y")), Divide(Variable("z"), Variable("w")))
+  }
+
+  it should "disallow improper quotients of actual units" in {
+    val input =
+      """
+        |ProgramUnits.
+        | U m.
+        | U n.
+        |End.
+        |ProgramVariables.
+        | R x : m.
+        | R y : n.
+        | R z : m.
+        | R w : n.
+        |End.
+        |Problem.
+        | x / y = w / z
+        |End.
+      """.stripMargin
+    intercept[ParseException] {
+      val (_, _) = KeYmaeraXProblemParser(input)
+    }
+  }
+
+  it should "allow equality of divided-out units" in {
+    val input =
+      """
+        |ProgramUnits.
+        | U m.
+        | U n.
+        |End.
+        |ProgramVariables.
+        | R x : m.
+        | R y : m.
+        | R n : n.
+        |End.
+        |Problem.
+        | x != (y*n)/n
+        |End.
+      """.stripMargin
+    val (result, _) = KeYmaeraXProblemParser(input)
+    result shouldBe NotEqual(Variable("x"), Divide(Times(Variable("y"), Variable("n")), Variable("n")))
+  }
+
+  it should "disallow equality of improperly divided-out units" in {
+    val input =
+      """
+        |ProgramUnits.
+        | U m.
+        | U n.
+        |End.
+        |ProgramVariables.
+        | R x : m.
+        | R y : m.
+        | R n : n.
+        |End.
+        |Problem.
+        | x != (y*x)/n
+        |End.
+      """.stripMargin
+    intercept[ParseException] {
+      val (result, _) = KeYmaeraXProblemParser(input)
+    }
+  }
+
 }
